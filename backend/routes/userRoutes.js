4d1acc7d895160c6ac9966d1abce43240a22235a
Mariya Zaveri <mariyazaveri13@gmail.com>
2024-05-20 18:42:02 +0530

    Auth middleware and endpoint authentication for admin and normal user implemented

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,13 +10,21 @@ import {
   getUsersbyID,
   updateUserbyID,
 } from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
-router.route('/profile').put(updateUserProfile).get(getUserProfile);
-router.route('/:id').put(updateUserbyID).get(getUsersbyID).delete(deleteUser);
+router
+  .route('/profile')
+  .put(protect, updateUserProfile)
+  .get(protect, getUserProfile);
+router
+  .route('/:id')
+  .put(protect, admin, updateUserbyID)
+  .get(protect, admin, getUsersbyID)
+  .delete(protect, admin, deleteUser);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000;
 
@@ -14,6 +15,9 @@ const app = express();
 //body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//cookie parser middleware
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('API is running');
