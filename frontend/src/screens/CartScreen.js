ac3e2b0845bd521f1731fb147bbfae96a2d761c0
Mariya Zaveri <mariyazaveri13@gmail.com>
2024-05-20 13:02:43 +0530

    Cart functionality - delete item from frontend

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -12,7 +12,7 @@ import {
 import { FaTrash } from 'react-icons/fa';
 import Message from '../components/Message';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToCart } from '../slices/cartSlice';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 const CartScreen = () => {
   const navigate = useNavigate();
@@ -24,6 +24,14 @@ const CartScreen = () => {
 
   const addToCartHandler = async (product, qty) => {
     dispatch(addToCart({ ...product, qty }));
+  };
+
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const checkoutHandler = () => {
+    navigate('/login?redirect=/shipping');
   };
 
   return (
@@ -72,7 +80,8 @@ const CartScreen = () => {
                   <Col md={2}>
                     <Button
                       type='button'
-                      variant='light'>
+                      variant='light'
+                      onClick={() => removeFromCartHandler(item._id)}>
                       <FaTrash></FaTrash>
                     </Button>
                   </Col>
@@ -99,7 +108,8 @@ const CartScreen = () => {
               <Button
                 type='button'
                 className='btn-block'
-                disabled={cartItems.length === 0}>
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}>
                 Proceed to checkout
               </Button>
             </ListGroup.Item>
