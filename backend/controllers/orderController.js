5fdcfd7723c67c806355ab728ab253ee91719dd8
Mariya Zaveri <mariyazaveri13@gmail.com>
2024-05-23 14:26:16 +0530

    configuring paypal

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -81,14 +81,30 @@ const getOrderByID = asyncHandler(async (req, res) => {
 });
 
 //@desc Update order to paid
-//@route GET /api/orders/:id/pay
+//@route PUT /api/orders/:id/pay
 //@Access Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send('update order to paid');
+  const order = await Order.findById(req.params.id);
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.email_address,
+    };
+
+    const updatedOrder = await order.save();
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('order not found');
+  }
 });
 
 //@desc Update order to delivered
-//@route GET /api/orders/:id/deliver
+//@route PUT /api/orders/:id/deliver
 //@Access Private/admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send('update order to delivered');
