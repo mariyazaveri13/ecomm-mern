26191ff2445f8cbb96418ba2b575c6b4cccf20ba
Mariya Zaveri <mariyazaveri13@gmail.com>
2024-05-27 20:04:11 +0530

    Finishing

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 //cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+// app.get('/', (req, res) => {
+//   res.send('API is running');
+// });
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -37,6 +37,22 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  console.log('in if');
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  //any route that is not api will be redirected to index.html
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  console.log('in else');
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
